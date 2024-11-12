ae5412bc8a5d5b318b8606d8a16a42e058eb23a3
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-12 15:23:36 -0500

    Add php files, forked from CAPANDA

---
+++ src/email-config.php
@@ -0,0 +1,6 @@
+<?php
+$mailboxEmail = "mail@example.com";      // Email address messages are sent to (required)
+// $fromEmail = "";                      // Email address messages are sent from (optional, defaults to $mailboxEmail)
+// $replyToEmail = "";                   // Reply-to email address included in confirmation emails (optional, defaults to $mailboxEmail)
+// $siteDomain = "";                     // Domain name of your site (optional, defaults to the hostname)
+// $siteName = "";                       // Name of your site (optional, defaults to $siteDomain capitalized with the TLD removed)
